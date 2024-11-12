b8ef9bf198e39071b6e352ef2be7788024de394c
Corax73 <ramaz999@inbox.ru>
2024-11-13 00:33:01 +0400

    working with date search

--- go/main.go
+++ go/main.go
@@ -3,16 +3,26 @@ package main
 import (
 	"fmt"
 	"math"
+	"regexp"
+	"slices"
+	"time"
 )
 
+type Solutions struct{}
+
 func main() {
+	solution := Solutions{}
+	fmt.Println(solution.findWeekend( /*"05.05.2024", "25.05.2024"*/ "2024-05-05", "2024-05-25"))
+}
+
+func (solution *Solutions) getBinarySearch() {
 	slice := []int{4, 8, 15, 15, 16, 23, 42}
 	searchedInt := 15
-	fmt.Println(binarySearch(slice, searchedInt))
+	fmt.Println(solution.binarySearch(slice, searchedInt))
 }
 
 // binarySearch searches the passed array for the key of the passed value. If input checks fail or there is no number in the array, returns -1.
-func binarySearch(array []int, searchedInt int) int {
+func (solutions *Solutions) binarySearch(array []int, searchedInt int) int {
 	resp := -1
 	end := float64(len(array) - 1)
 	if end+1 <= math.MaxInt {
@@ -32,6 +42,42 @@ func binarySearch(array []int, searchedInt int) int {
 			}
 			if resp > -1 {
 				break
+			}
+		}
+	}
+	return resp
+}
+
+func (solutions *Solutions) findWeekend(begin, end string) int {
+	resp := -1
+	_, err := regexp.Compile(`06.06.2020`)
+	if err != nil {
+		fmt.Println(err)
+	}
+	matchedBegin, _ := regexp.MatchString(`\d+.\d+.\d+`, begin)
+	matchedEnd, _ := regexp.MatchString(`\d+.\d+.\d+`, end)
+	if matchedBegin && matchedEnd {
+		timeBegin, err := time.Parse(time.DateOnly, begin)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			timeEnd, err := time.Parse(time.DateOnly, end)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				if timeBegin.Unix() <= math.MaxInt && timeEnd.Unix() <= math.MaxInt {
+					resp = 0
+					weekendsNumbers := []int{5, 6}
+					for timeBegin.Unix() <= timeEnd.Unix() {
+						if slices.Contains(weekendsNumbers, int(timeBegin.Weekday())) {
+							resp++
+							timeBegin = timeBegin.Add(24 * time.Hour)
+						} else {
+							hours := 24 * (5 - int(timeBegin.Weekday()))
+							timeBegin = timeBegin.Add(time.Duration(hours) * time.Hour)
+						}
+					}
+				}
 			}
 		}
 	}
